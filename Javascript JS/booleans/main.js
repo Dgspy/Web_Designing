7ee76433b7b9538c56a61b14658345efbe6fb154
Cshacker <106894304+Dgspy@users.noreply.github.com>
2022-06-08 16:03:37 +0530

    Add files via upload

---
+++ Javascript JS/booleans/main.js
@@ -0,0 +1,61 @@
+console.log('Hello World!');
+var isActive = true;
+var isHoliday = false;
+document.write(isActive,'\r');
+document.write(isHoliday,"\n");
+var x = 10 + 5;
+document.write(x,"\n");
+var multiply = 10 * 5;
+document.write(multiply,"\n");
+var sub = 10 - 5;
+document.write(sub, "\n");
+var divide = 10 / 5;
+document.write(divide, "\n");
+var module = 25 % 5;
+document.write(module, "\n");
+var num = 10;
+document.write(num == 8);
+document.write(10>5,"\n");
+document.write(10<5,"\n");
+document.write(10>=5,"\n");
+document.write(10<=5,"\n");
+document.write(10==5,"\n");
+document.write(10!=5,"\n");
+//logical operator
+var a = (4>2) && (10 < 15);
+document.write(a,"\n");
+var b = (4>2) || (10 < 15);
+document.write(b,"\n");
+var c = (4>2) != (10 < 15);
+document.write(c,"\n");
+//string operand
+var string = "I am a good boy from";
+var string2 = "village ralegan";
+document.write(string + string2,"\n")
+// if condition statement
+var num1 = 7;
+var num2 = 10;
+if (num1 < num2) {
+  document.write("javascript is easy to learn","\n");
+}
+var num1 = 7;
+var num2 = 10;
+if (num1 > num2) {
+  alert("javascript is soft to learn");
+}
+var a = 10;
+var b = 20;
+if (a < b) {
+  document.write("a is lesser than b");
+}
+else {
+  document.write("a is greater then b");
+}
+var a = 20;
+var b = 10;
+if (a < b) {
+  document.write("a is lesser than b");
+}
+else {
+  document.write("a is greater then b");
+}
