399680983b07bbd6f096f67e0bcadc484546d704
Cshacker <106894304+Dgspy@users.noreply.github.com>
2022-06-08 16:12:30 +0530

    Add files via upload

---
+++ Javascript JS/else if/main.js
@@ -0,0 +1,55 @@
+console.log('Hello World!');
+var num = 1;
+if (num == 1){
+  document.write("<h1>JavaScript Toturial</h1>");
+}else if (num == 2){
+  document.write("<h1>Css Toturial</h1>");
+}else{
+  document.write("<h1>Html Toturial</h1>");
+}
+
+var num = 3;
+if (num == 1){
+  document.write("<h1>JavaScript Toturial</h1>");
+}else if (num == 2){
+  document.write("<h1>Css Toturial</h1>");
+}else{
+  document.write("<h1>Html Toturial</h1>");
+}
+// switch statement
+var day = 2;
+switch(day){
+  case 1 :
+    document.write("monday");
+    break;
+  case 2:
+    document.write("<b>tuesday</b>");
+    break;
+  case 3:
+    document.write("wednesday");
+    break;
+  default :
+  document.write("sunday");
+}
+var x = 3;
+switch(x){
+  case 1:
+    document.write(x);
+    break;
+  case 2:
+    document.write(x+2);
+    break;
+  default:
+  document.write(x+5);
+}
+var color = "blue";
+switch(color){
+  case "green":
+    document.write("<i>this is green</i>");
+    break;
+  case "blue":
+    document.write("<i>this is blue</i>");
+    break;
+  default:
+    document.write("color is not found")
+}
